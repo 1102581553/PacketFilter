4a4457de4bb34de303267d7cff835031b0c8d3de
1102581553 <pheyeji@gmail.com>
2026-02-24 07:33:21 +0000

    Create PacketFilterPlugin.h

---
+++ src/PacketFilterPlugin.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <mc/deps/raknet/PluginInterface2.h>
+#include <mc/deps/raknet/PluginReceiveResult.h>
+#include <mc/deps/raknet/Packet.h>
+#include <cstring>
+
+namespace packet_filter {
+
+// Packet 布局偏移，length 字段在 +152
+static constexpr size_t kPacketLengthOffset = 152;
+
+class PacketFilterPlugin : public RakNet::PluginInterface2 {
+public:
+    explicit PacketFilterPlugin(uint minSize) : mMinSize(minSize) {}
+
+    ::RakNet::PluginReceiveResult OnReceive(::RakNet::Packet* packet) override {
+        if (!packet) return ::RakNet::PluginReceiveResult::StopProcessingAndDeallocate;
+        uint len = 0;
+        std::memcpy(&len, reinterpret_cast<char*>(packet) + kPacketLengthOffset, sizeof(uint));
+        if (len < mMinSize) return ::RakNet::PluginReceiveResult::StopProcessingAndDeallocate;
+        return ::RakNet::PluginReceiveResult::ContinueProcessing;
+    }
+
+    void setMinSize(uint s) { mMinSize = s; }
+
+private:
+    uint mMinSize;
+};
+
+} // namespace packet_filter
